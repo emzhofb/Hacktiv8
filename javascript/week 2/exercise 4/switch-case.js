b3b119352939cec6a08a15554034304c70090a19
Ikhda Muhammad Wildani <43491260+emzhofb@users.noreply.github.com>
2018-11-22 09:21:30 +0700

    Update switch-case.js

--- javascript/week 2/exercise 4/switch-case.js
+++ javascript/week 2/exercise 4/switch-case.js
@@ -1,6 +1,6 @@
-var tanggal = 1;
-var bulan = 2;
-var tahun = 2001;
+var tanggal = 23;
+var bulan = 15;
+var tahun = 1999;
 
 switch (bulan) {
     case 1:
@@ -36,9 +36,24 @@ switch (bulan) {
     case 11:
         bulan = 'November'
     break;
-    default:
+    case 12:
         bulan = 'Desember'
+    break;
+    default:
+        bulan = 'inputlah bulan yang benar'
         break;
 }
 
-console.log(tanggal + ' ' + bulan + ' ' + tahun)
+// if cek tanggal
+if (tanggal <= 0 || tanggal > 31){
+    console.log('inputlah tanggal yang benar')
+}
+// else if untuk tahun
+else if (tahun < 1900 || tahun > 2200){
+    console.log('inputlah tahun di antara 1900 sampai 2200')
+}
+
+// else 
+else{
+    console.log(tanggal + ' ' + bulan + ' ' + tahun)
+}
