97cfef7414338f828ecfd9ae18d0425e2d309204
emzhofb <ikhdamuhammad@gmail.com>
2018-12-10 00:05:14 +0700

    update

--- javascript/week 4/06.js
+++ javascript/week 4/06.js
@@ -1,5 +1,30 @@
 function digitPerkalianMinimum(angka) {
   // you can only write your code here!
+  var tampung = [];
+
+  for( let i = 1; i <= angka; i++ ) {
+    // console.log(i)
+    for( let j = i; j <= angka; j++ ) {
+      // console.log(j)
+      // console.log(i*j)
+      if(i*j === angka) {
+        // console.log('masuk pak eko', i,j)
+        // var num = String(angka[i]) + String(angka[j])
+        // console.log(String(angka[i]))
+        // console.log(toString(i,j))
+        var num = i + '' + j
+        tampung.push(num)
+      }
+    }
+  }
+  var jumlah = 0
+  for( let k = 0; k < tampung.length; k++ ) {
+    if(tampung[k].length <= tampung[0].length) {
+      // jumlah++
+      jumlah = tampung[k].length
+    }
+  }
+  return jumlah
 }
 
 // TEST CASES
